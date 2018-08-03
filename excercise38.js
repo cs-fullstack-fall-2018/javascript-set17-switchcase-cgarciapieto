06150e6023e622d6c361344751d6832dcc0381f9
Carlos Garcia <cgarciapieto@gmail.com>
2018-08-03 13:15:20 -0500

    javascript-set17-switchcase-cgarciapieto.iml

---
+++ excercise38.js
@@ -0,0 +1,31 @@
+function main() {
+
+    var num = parseInt(prompt("Enter a number"));
+    switch(num)
+    {
+    case 1:
+
+        console.log("1");
+        break;
+
+    case 2:
+
+        console.log("2");
+        break;
+
+    case 3:
+
+        console.log("3");
+        break;
+    }
+
+
+}
+
+
+////case code for a user inputing a number and printing to console if user inouts 1,2or 3
+
+
+
+
+main();
---
+++ excercise40.js
@@ -0,0 +1,37 @@
+function main() {
+
+    var grade = prompt("Enter a Grade");
+    switch(grade)
+    {
+        case 1:
+
+            alert("A");
+            break;
+
+        case 2:
+
+            alert("B");
+            break;
+
+        case 3:
+
+            alert("C");
+            break;
+
+        case 4:
+
+            alert("D");
+            break;
+
+    }
+
+
+}
+
+
+
+
+
+
+
+main();
